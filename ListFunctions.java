a0b7ccdc27beafdff64e115c8e82e885b313e832
TannerDryden <tdd7197@gmail.com>
2020-09-18 20:49:28 -0700

    added functions to find max min and median times

---
+++ ListFunctions.java
@@ -0,0 +1,43 @@
+import java.util.LinkedList;
+
+
+public class ListFunctions {
+
+
+    // find max long variable in a linked list
+    public static Long findMaxLong(LinkedList<Long> list) {
+        int size = list.size();
+        long max = list.get(0);
+        for (int i = 0; i < size; i++) {
+            if (max < list.get(i)) {
+                max = list.get(i);
+            }
+        }
+        return max;
+    }
+    // find min long variable in a linked list
+    public static Long findMinLong(LinkedList<Long> list) {
+        int size = list.size();
+        long min = list.get(0);
+        for (int i = 1; i < size; i++) {
+            if (min < list.get(i)) {
+                min = list.get(i);
+            }
+        }
+        return min;
+    }
+
+    // find median int variable in a linked list
+    public static Integer findMedInt(LinkedList<Integer> list) {
+        int size = list.size();
+        int med = size/2;
+        return list.get(med);
+    }
+
+    // find median long variable in a linked list
+    public static Long findMedLong (LinkedList <Long> list) {
+        int size = list.size();
+        int med = size/2;
+        return list.get(med);
+    }
+}
--- loadList.java
+++ loadList.java
@@ -13,10 +13,18 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+
+
+
 public class loadList {
 
     public static void main(String[] arg) {
-        LinkedList list = new LinkedList();
+        LinkedList<Integer> list = new LinkedList<>();
+        LinkedList<Long> timeList = new LinkedList<>();
+
+        long timeStart, timeEnd, insertStart, insertEnd;
+        long time_insert = 0;
+
 
         try {
             // Gets file name from user
@@ -29,25 +37,58 @@ public class loadList {
 
             // creates a reader for the file
             File inputFile = new File(path.toURI());
+
             Scanner fileReader = new Scanner(inputFile);
 
-            // adds data to the list as the file is read
+            // adds data to the list and sorts as the file is read
+            insertStart = System.nanoTime();
+
             while(fileReader.hasNextLine()) {
+                timeStart = System.nanoTime();
+
                 String stringData = fileReader.nextLine();
                 list.add(Integer.parseInt(stringData));
+
+                // Sort the list low -> high
+                Collections.sort(list);
+
+                timeEnd = System.nanoTime();
+
+                // record times for each insertion
+                timeList.add(timeEnd - timeStart);
             }
+            insertEnd = System.nanoTime();
             fileReader.close();
 
-            // Sort the list low -> high
-            Collections.sort(list);
+            // report total time for insertion
+            time_insert = insertEnd - insertStart;
 
             // exception handling
-        } catch (URISyntaxException | FileNotFoundException error) {
-            System.out.println("File not in the directory");
-            error.printStackTrace();
-        }
+             } catch (URISyntaxException | FileNotFoundException error) {
+               System.out.println("File not in the directory");
+               error.printStackTrace();
+                }
+
+        Collections.sort(timeList);
 
         // prints list for testing
         System.out.println(list);
+        System.out.println(timeList);
+
+        // min number of the list
+        System.out.println("The min number is: " + list.getFirst());
+        // med number of the list
+        System.out.println("The median number is: " + ListFunctions.findMedInt(list));
+        // max number of the list
+        System.out.println("The max number is: " + list.getLast());
+
+        // finds min time insert
+        System.out.println("The min time was " + ListFunctions.findMinLong(timeList) + " nanoseconds");
+        // finds med time insert
+        System.out.println("The median time was " + ListFunctions.findMedLong(timeList) + " nanoseconds");
+        // finds max time insert
+        System.out.println("The max time was " + ListFunctions.findMaxLong(timeList) + " nanoseconds");
+        // time to insert
+        System.out.println("The total time to insert is: " + time_insert + " nanoseconds");
     }
 }
